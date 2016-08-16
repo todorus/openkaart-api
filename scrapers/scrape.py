5f379f116504b7cefd181d42d31ffe3fc189fd97
Theo van den Bogaart <todorus@todorus.com>
2016-08-16 20:11:19 +0200

    removed scrapers dependencies and added an BAG scraper

--- scrapers/scrape.py
+++ scrapers/scrape.py
@@ -1,17 +1,27 @@
-from owslib.wfs import WebFeatureService
+from urllib import urlopen
 
 
-def scrape(typename, filename):
-    wfs11 = WebFeatureService(url='https://geodata.nationaalgeoregister.nl/cbsgebiedsindelingen/ows', version='1.1.0')
-    response = wfs11.getfeature(typename=typename, outputFormat='json', srsname='urn:x-ogc:def:crs:EPSG:4326')
-    out = open(filename, 'wb')
+def scrape(serverBaseUrl, typeName, fileName):
+    url = '{}?SERVICE=WFS&request=getFeature&typeName={}&outputFormat=json&srsName=urn:x-ogc:def:crs:EPSG:4326'.format(serverBaseUrl, typeName)
+
+    print 'fetching: {}'.format(url)
+    response = urlopen(url)
+
+    print 'writing: {}'.format(fileName)
+    out = open(fileName, 'wb')
     out.write(bytes(response.read()))
     out.close()
 
-scrape('cbsgebiedsindelingen:cbs_provincie_2016_gegeneraliseerd', 'provinces.geo.json')
-scrape('cbsgebiedsindelingen:cbs_gemeente_2016_gegeneraliseerd', 'municipalities.geo.json')
-scrape('cbsgebiedsindelingen:cbs_wijk_2016_gegeneraliseerd', 'district.geo.json')
-scrape('cbsgebiedsindelingen:cbs_buurt_2016_gegeneraliseerd', 'neighborhoods.geo.json')
 
-scrape('cbsgebiedsindelingen:cbs_ggdregio_2016_gegeneraliseerd', 'ggd-regions.geo.json')
-scrape('cbsgebiedsindelingen:cbs_jeugdzorgregio_2016_gegeneraliseerd', 'ggd-regions.geo.json')
+cbsRegionsWFS = 'https://geodata.nationaalgeoregister.nl/cbsgebiedsindelingen/ows'
+scrape(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_provincie_2016_gegeneraliseerd', 'provinces.geo.json')
+scrape(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_gemeente_2016_gegeneraliseerd', 'municipalities.geo.json')
+scrape(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_wijk_2016_gegeneraliseerd', 'district.geo.json')
+scrape(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_buurt_2016_gegeneraliseerd', 'neighborhoods.geo.json')
+scrape(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_ggdregio_2016_gegeneraliseerd', 'ggd.geo.json')
+scrape(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_jeugdzorgregio_2016_gegeneraliseerd', 'youthcare.geo.json')
+
+bagAddressWFS = 'https://geodata.nationaalgeoregister.nl/inspireadressen/wfs'
+scrape(bagAddressWFS, 'inspireadressen:inspireadressen', 'adressen.geo.json')
+
+# Postgres and PostGis are required for the next steps
