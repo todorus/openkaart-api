6f243e7469d2614eae573a3ba5aa96f3b6c873af
Theo van den Bogaart <todorus@todorus.com>
2016-08-20 22:41:31 +0200

    made scraper looping instead of recursive
    recursive was asking for a stackoverflow on big datasets

--- scrapers/scrape.py
+++ scrapers/scrape.py
@@ -1,16 +1,14 @@
 from urllib import urlopen
 import json
-from math import floor
+from math import ceil
 import time
 import datetime
 
 def days_hours_minutes(timeDelta):
     return timeDelta.days, timeDelta.seconds//3600, (timeDelta.seconds//60)%60
 
-# https://geodata.nationaalgeoregister.nl/inspireadressen/wfs?version=2.0.0&request=getFeature&typeName=inspireadressen:inspireadressen&outputFormat=json&srsName=urn:x-ogc:def:crs:EPSG:4326&count=10&startIndex=0&sortBy=gid
-def scrape(serverBaseUrl, typeName, fileName, startIndex, total, sortBy):
-    limit = 1000
-
+def scrape(serverBaseUrl, typeName, fileName, page, limit, total, sortBy):
+    startIndex = page * limit
     if(sortBy is None):
         url = '{}?SERVICE=WFS&request=getFeature&typeName={}&outputFormat=json&srsName=urn:x-ogc:def:crs:EPSG:4326&count={}&startIndex={}'.format(serverBaseUrl, typeName, limit, startIndex)
     else:
@@ -24,35 +22,47 @@ def scrape(serverBaseUrl, typeName, fileName, startIndex, total, sortBy):
         data = json.load(response)
         total = data["totalFeatures"]
 
-    index = (startIndex / limit)
-    indexedFilename = "%s.%d" % (fileName, index)
+    page = (startIndex / limit)
+    indexedFilename = "%s.%d" % (fileName, page)
 
     print 'writing: %s' % indexedFilename
     out = open(indexedFilename, 'wb')
     out.write(bytes(response.read()))
     out.close()
 
-    lastIndex = startIndex + limit
+    passedTime = time.time() - startTime
+
+    return page, total, passedTime
 
 
-    if lastIndex < total:
-        passedTime = time.time() - startTime
-        totalIndex = floor(total / limit)
-        indexesLeft = totalIndex - index
-        timeDelta = datetime.timedelta(seconds=indexesLeft * passedTime)
-        timeString = ", estimated time remaining: %s" % timeDelta
+def start_scrape(serverBaseUrl, typeName, fileName, sortBy):
+    page = 0
+    limit = 1000
+    totalPages = 1
+    total = 0
 
-        print "moving to %s (%d/%d) %s" % (typeName, index+1, totalIndex, timeString)
-        scrape(serverBaseUrl, typeName, fileName, startIndex+limit, total, sortBy)
+    while page < totalPages:
+        page, total, passedTime = scrape(serverBaseUrl, typeName, fileName, page, limit, total, sortBy)
+
+        if page == 0:
+            totalPages = ceil(total / limit)
+
+        if page < totalPages:
+            indexesLeft = totalPages - page
+            timeDelta = datetime.timedelta(seconds=indexesLeft * passedTime)
+            timeString = ", estimated time remaining: %s" % timeDelta
+
+            page += 1
+            print "moving to %s (%d/%d) %s" % (typeName, page, totalPages, timeString)
 
 
 cbsRegionsWFS = 'https://geodata.nationaalgeoregister.nl/cbsgebiedsindelingen/ows'
-scrape(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_provincie_2016_gegeneraliseerd', '../data/provinces.geo.json', 0, 0, None)
-scrape(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_gemeente_2016_gegeneraliseerd', '../data/municipalities.geo.json', 0, 0, None)
-scrape(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_wijk_2016_gegeneraliseerd', '../data/district.geo.json', 0, 0, None)
-scrape(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_buurt_2016_gegeneraliseerd', '../data/neighborhoods.geo.json', 0, 0, None)
-scrape(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_ggdregio_2016_gegeneraliseerd', '../data/ggd.geo.json', 0, 0, None)
-scrape(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_jeugdzorgregio_2016_gegeneraliseerd', '../data/youthcare.geo.json', 0, 0, None)
+start_scrape(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_provincie_2016_gegeneraliseerd', '../data/provinces.geo.json', None)
+start_scrape(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_gemeente_2016_gegeneraliseerd', '../data/municipalities.geo.json', None)
+start_scrape(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_wijk_2016_gegeneraliseerd', '../data/district.geo.json', None)
+start_scrape(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_buurt_2016_gegeneraliseerd', '../data/neighborhoods.geo.json', None)
+start_scrape(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_ggdregio_2016_gegeneraliseerd', '../data/ggd.geo.json', None)
+start_scrape(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_jeugdzorgregio_2016_gegeneraliseerd', '../data/youthcare.geo.json', None)
 
 bagAddressWFS = 'https://geodata.nationaalgeoregister.nl/inspireadressen/wfs'
-scrape(bagAddressWFS, 'inspireadressen:inspireadressen', '../data/adressen.geo.json', 0, 0, "gid")
+start_scrape(bagAddressWFS, 'inspireadressen:inspireadressen', '../data/adressen.geo.json', "gid")
