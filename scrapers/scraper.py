b97f5bd5295f5be439d7a12e7c75cc94a285467b
Theo van den Bogaart <todorus@todorus.com>
2016-08-21 19:04:01 +0200

    split up functionality of scraper to allow for more customization

--- scrapers/scraper.py
+++ scrapers/scraper.py
@@ -11,8 +11,8 @@ def days_hours_minutes(timeDelta):
 
 class Scraper():
 
-    def __init__(self, serverBaseUrl, typeName, fileName, sortBy):
-        self.serverBaseUrl = serverBaseUrl
+    def __init__(self, basePath, typeName, fileName, sortBy):
+        self.basePath = basePath
         self.typeName = typeName
         self.fileName = fileName
         self.sortBy = sortBy
@@ -28,7 +28,7 @@ class Scraper():
 
         while page < totalPages:
             try:
-                page, total, passedTime = self.scrape(self.serverBaseUrl, self.typeName, page, limit, total, self.sortBy)
+                total, passedTime = self.scrape(page, limit, total)
 
                 if page == 0:
                     totalPages = ceil(total / limit)
@@ -51,45 +51,38 @@ class Scraper():
                     print "more than %d failed attempts, aborting" % maxAttempts
                     break
 
-    def scrape(self, serverBaseUrl, typeName, page, limit, total, sortBy):
-        startIndex = page * limit
-        if(sortBy is None):
-            url = '{}?SERVICE=WFS&request=getFeature&typeName={}&outputFormat=json&srsName=urn:x-ogc:def:crs:EPSG:4326&count={}&startIndex={}'.format(serverBaseUrl, typeName, limit, startIndex)
-        else:
-            url = '{}?SERVICE=WFS&request=getFeature&typeName={}&outputFormat=json&srsName=urn:x-ogc:def:crs:EPSG:4326&count={}&startIndex={}&sortBy={}'.format(serverBaseUrl, typeName, limit, startIndex, sortBy)
+    def scrape(self, page, limit, total):
 
         startTime = time.time()
-        print 'fetching: %s' % url
-        response = urlopen(url)
-        content = response.read()
 
+        content = self.fetch(page, limit)
         total = self.write(content, page)
-        page = (startIndex / limit)
 
         passedTime = time.time() - startTime
 
-        return page, total, passedTime
+        return total, passedTime
 
-    def write(self, content, page):
+    def fetch(self, page, limit):
+        startIndex = page * limit
+        if(self.sortBy is None):
+            url = '{}?SERVICE=WFS&request=getFeature&typeName={}&outputFormat=json&srsName=urn:x-ogc:def:crs:EPSG:4326&count={}&startIndex={}'.format(self.basePath, self.typeName, limit, startIndex)
+        else:
+            url = '{}?SERVICE=WFS&request=getFeature&typeName={}&outputFormat=json&srsName=urn:x-ogc:def:crs:EPSG:4326&count={}&startIndex={}&sortBy={}'.format(self.basePath, self.typeName, limit, startIndex, self.sortBy)
+
+        print 'fetching: %s' % url
+        response = urlopen(url)
+        content = response.read()
+        return content
+
+
+    def write(self, json_string, page):
         indexedFilename = "%s.%d" % (self.fileName, page)
 
         print 'writing: %s' % indexedFilename
         out = open(indexedFilename, 'wb')
-        out.write(bytes(content))
+        out.write(bytes(json_string))
         out.close()
 
-        data = json.loads(content)
+        data = json.loads(json_string)
         total = data["totalFeatures"]
         return total
-
-
-cbsRegionsWFS = 'https://geodata.nationaalgeoregister.nl/cbsgebiedsindelingen/ows'
-Scraper(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_provincie_2016_gegeneraliseerd', '../data/provinces.geo.json', None).start()
-Scraper(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_gemeente_2016_gegeneraliseerd', '../data/municipalities.geo.json', None).start()
-Scraper(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_wijk_2016_gegeneraliseerd', '../data/district.geo.json', None).start()
-Scraper(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_buurt_2016_gegeneraliseerd', '../data/neighborhoods.geo.json', None).start()
-Scraper(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_ggdregio_2016_gegeneraliseerd', '../data/ggd.geo.json', None).start()
-Scraper(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_jeugdzorgregio_2016_gegeneraliseerd', '../data/youthcare.geo.json', None).start()
-
-bagAddressWFS = 'https://geodata.nationaalgeoregister.nl/inspireadressen/wfs'
-Scraper(bagAddressWFS, 'inspireadressen:inspireadressen', '../data/adressen.geo.json', "gid").start()
