96297199046c778567c4a358dab036a27b6459ac
Theo van den Bogaart <todorus@todorus.com>
2016-08-21 18:17:23 +0200

    scrapers can handle multiple json files

--- scrapers/relate_areas.py
+++ scrapers/relate_areas.py
@@ -2,6 +2,38 @@ import json
 import geojson
 import psycopg2
 from shapely.geometry import shape
+import os.path
+
+def loader(fileNameBase):
+    index = 0
+    fileName = "%s.%d" % (fileNameBase, index)
+
+    while(os.path.isfile(fileName)):
+        load(fileName)
+        index += 1
+        fileName = "%s.%d" % (fileNameBase, index)
+
+
+def load(fileName):
+    print "loading: %s" % fileName
+    data = json.load(open(fileName))
+    features = data["features"]
+    for feature in features:
+        properties = feature["properties"]
+
+        geometry_dump = json.dumps(feature["geometry"])
+        geometry_geojson = geojson.loads(geometry_dump)
+        geometry_wkt = shape(geometry_geojson).wkt
+
+        cur.execute("""
+        INSERT INTO postalcodes_to_municipalities (municipality_id, postalcode_id)
+        SELECT municipalities.id, postal_codes.id
+        FROM municipalities, postal_codes
+        WHERE ST_CONTAINS(municipalities.geometry, ST_GeomFromText(%s))
+        AND postal_codes.name = %s
+        ON CONFLICT DO NOTHING
+        """, (geometry_wkt, properties["postcode"][0:4]))
+    conn.commit()
 
 conn = psycopg2.connect("dbname=openkaart_development user=todorus")
 cur = conn.cursor()
@@ -9,27 +41,7 @@ cur = conn.cursor()
 cur.execute("DROP TABLE IF EXISTS postalcodes_to_municipalities")
 cur.execute('CREATE TABLE postalcodes_to_municipalities (id serial PRIMARY KEY, municipality_id integer, postalcode_id integer, UNIQUE (municipality_id, postalcode_id))')
 
-data = json.load(open("adressen.geo.json"))
-features = data["features"]
-for feature in features:
-    properties = feature["properties"]
-
-    geometry_dump = json.dumps(feature["geometry"])
-    geometry_geojson = geojson.loads(geometry_dump)
-    geometry_wkt = shape(geometry_geojson).wkt
-
-    print properties
-
-    cur.execute("""
-    INSERT INTO postalcodes_to_municipalities (municipality_id, postalcode_id)
-    SELECT municipalities.id, postal_codes.id
-    FROM municipalities, postal_codes
-    WHERE ST_CONTAINS(municipalities.geometry, ST_GeomFromText(%s))
-    AND postal_codes.name = %s
-    ON CONFLICT DO NOTHING
-    """, (geometry_wkt, properties["postcode"][0:4]))
-
-conn.commit()
+loader("../data/adressen.geo.json")
 
 cur.close()
 conn.close()
--- scrapers/seed_areas.py
+++ scrapers/seed_areas.py
@@ -2,6 +2,37 @@ import json
 import geojson
 import psycopg2
 from shapely.geometry import shape
+import os.path
+
+
+def loader(fileNameBase):
+    index = 0
+    fileName = "%s.%d" % (fileNameBase, index)
+
+    while(os.path.isfile(fileName)):
+        load(fileName)
+        index += 1
+        fileName = "%s.%d" % (fileNameBase, index)
+
+
+def load(fileName):
+    print "loading: %s" % fileName
+    data = json.load(open(fileName))
+    features = data["features"]
+    for feature in features:
+        properties = feature["properties"]
+        code = int(properties["statcode"][2:])
+        name = properties["statnaam"]
+
+        geometry_dump = json.dumps(feature["geometry"])
+        geometry_geojson = geojson.loads(geometry_dump)
+        geometry_wkt = shape(geometry_geojson).wkt
+
+        data = (code, name, geometry_wkt)
+
+        cur.execute("INSERT INTO municipalities (code, name, geometry) VALUES (%s, %s, ST_GeomFromText(%s))", data)
+        print "inserted municipality %s, %s" % (code, name)
+    conn.commit()
 
 regions_table_name = "regions"
 
@@ -12,32 +43,17 @@ cur = conn.cursor()
 print "municipalities"
 cur.execute("DROP TABLE IF EXISTS municipalities")
 cur.execute('CREATE TABLE municipalities (id serial PRIMARY KEY, code integer, name varchar, "geometry" geometry)')
+loader("../data/municipalities.geo.json")
 
-data = json.load(open("municipalities.geo.json"))
-features = data["features"]
-for feature in features:
-    properties = feature["properties"]
-    code = int(properties["statcode"][2:])
-    name = properties["statnaam"]
-
-    geometry_dump = json.dumps(feature["geometry"])
-    geometry_geojson = geojson.loads(geometry_dump)
-    geometry_wkt = shape(geometry_geojson).wkt
-
-    data = (code, name, geometry_wkt)
-
-    cur.execute("INSERT INTO municipalities (code, name, geometry) VALUES (%s, %s, ST_GeomFromText(%s))", data)
-    print "inserted municipality %s, %s" % (code, name)
-conn.commit()
 
 print "postal codes"
-cur.execute("DROP TABLE IF EXISTS postal_codes")
-cur.execute("CREATE TABLE postal_codes (id serial PRIMARY KEY, name varchar)")
+# cur.execute("DROP TABLE IF EXISTS postal_codes")
+# cur.execute("CREATE TABLE postal_codes (id serial PRIMARY KEY, name varchar)")
 
 for number in range(0,10000):
     name = str(number).zfill(4)
     data = (name,)
-    cur.execute("INSERT INTO postal_codes (name) VALUES (%s)", data)
+    cur.execute("INSERT INTO postal_codes (name) VALUES (%s) ON CONFLICT DO NOTHING", data)
     print "inserted postal area %s" % (name)
 conn.commit()
 
