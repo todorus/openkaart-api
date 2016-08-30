773f4d07f3348ab16a404e82677fbb3f26fa10de
Theo van den Bogaart <todorus@todorus.com>
2016-08-30 23:07:13 +0200

    using paths that are mountable by Docker for mac

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Set the base image to Ubuntu
-FROM ubuntu
+FROM ubuntu:16.04
 
 # File Author / Maintainer
 MAINTAINER todorus
@@ -14,7 +14,7 @@ RUN useradd -m scraper
 USER scraper
 WORKDIR /home/scraper
 RUN git clone -b feature/scraper https://github.com/todorus/openkaart-api.git
-RUN mkdir openkaart-api/data
+RUN mkdir -p /Volumes/openkaart_data/
 
 # Install Postgres and PostGis
 USER root
@@ -25,15 +25,23 @@ RUN sed -i 's/peer/trust/g' /etc/postgresql/9.5/main/pg_hba.conf
 # Configure database
 USER postgres
 RUN service postgresql start && \
-    psql -f openkaart-api/create_database.sql
+    psql -f openkaart-api/create_database.sql && \
+    service postgresql stop
 
 # Install Python and Basic Python Tools
 USER root
 RUN apt-get install -y python python-dev python-distribute python-pip libpq-dev
 RUN pip install -r openkaart-api/scrapers/requirements.txt
 
+# create the database tables
+RUN service postgresql start && \
+    psql -U postgres -d openkaart_development -c "CREATE EXTENSION postgis;" && \
+    python openkaart-api/scrapers/create_database_tables.py  && \
+    service postgresql stop
+
 # make sure postgres is running and start the scraper
 WORKDIR /home/scraper/openkaart-api/scrapers
-CMD service postgresql restart && \
+CMD service postgresql start && \
     git pull && \
     python run_scrapers.py
+VOLUME /volumes/openkaart-data/
--- scrapers/run_scrapers.py
+++ scrapers/run_scrapers.py
@@ -11,11 +11,11 @@ logging.getLogger().addHandler(ch)
 
 cbsRegionsWFS = 'https://geodata.nationaalgeoregister.nl/cbsgebiedsindelingen/ows'
 # WFSScraper(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_provincie_2016_gegeneraliseerd', '../data/provinces.geo.json', None).start()
-MunicipalityWFSScraper(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_gemeente_2016_gegeneraliseerd', '../data/municipalities.geo.json', None).start()
+MunicipalityWFSScraper(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_gemeente_2016_gegeneraliseerd', '/Volumes/openkaart_data/municipalities.geo.json', None).start()
 # WFSScraper(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_wijk_2016_gegeneraliseerd', '../data/district.geo.json', None).start()
 # WFSScraper(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_buurt_2016_gegeneraliseerd', '../data/neighborhoods.geo.json', None).start()
 # WFSScraper(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_ggdregio_2016_gegeneraliseerd', '../data/ggd.geo.json', None).start()
 # WFSScraper(cbsRegionsWFS, 'cbsgebiedsindelingen:cbs_jeugdzorgregio_2016_gegeneraliseerd', '../data/youthcare.geo.json', None).start()
 
 bagAddressWFS = 'https://geodata.nationaalgeoregister.nl/inspireadressen/wfs'
-AddressWFSScraper(bagAddressWFS, 'inspireadressen:inspireadressen', '../data/adressen.geo.json', "gid").start()
+AddressWFSScraper(bagAddressWFS, 'inspireadressen:inspireadressen', '/Volumes/openkaart_data/adressen.geo.json', "gid").start()
