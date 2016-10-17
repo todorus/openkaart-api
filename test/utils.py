6ab6fa25cd52e94097b71231a023876f57c6f4bd
Theo van den Bogaart <todorus@todorus.com>
2016-10-17 20:46:51 +0200

    removed legacy project structure

--- test/utils.py
+++
@@ -1,9 +0,0 @@
-import app.db.setup as db
-from unittest import TestCase
-
-
-TestCase.maxDiff = None
-
-
-def wipe_db(graph, databaseName):
-    graph.data("MATCH (n:"+databaseName+") OPTIONAL MATCH (n:"+databaseName+")-[r]->(m) DELETE n,m,r")
