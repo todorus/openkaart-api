919ce99417af7a4a2f7bb5b053d1f4768e174302
Theo van den Bogaart <todorus@todorus.com>
2016-10-18 07:28:39 +0200

    got a testcase running in a seperate test container

--- app/lib/db/setup.py
+++ app/lib/db/setup.py
@@ -1,20 +1,8 @@
-import app.lib.db.config as config
-from py2neo import Graph
-
-# example config. Note the hostname should be the same as the database
-# containers name in the docker-compose.yaml
-# "test": {
-#   "secure" : False,
-#   "host": "database",
-#   "http_port": 7474,
-#   "https_port": 7474,
-#
-#   "user" : "user",
-#   "password" : "pass"
-# }
-
-
 def init_graph(environment):
+    import app.lib.db.config as config
+    from py2neo import Graph
+    
     used_config = config.db_configs[environment]
+    print "used config %s" % (used_config)
     graph = Graph(**used_config)
     return graph
--- app/regions/test.sh
+++ app/regions/test.sh
@@ -1,8 +1,15 @@
 # get the local directory to convert relative paths to absolute ones
 LOCAL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# make sure we use the latest build
+# make sure we use the latest builds
 docker-compose -f $LOCAL_DIR/.docker/docker-compose-production.yaml build
+docker-compose -f $LOCAL_DIR/.docker/docker-compose-test.yaml build
 
-# run the testing container
+# run the production container
 docker-compose -f $LOCAL_DIR/.docker/docker-compose-production.yaml up
+
+# give the databse time to start
+sleep 10s
+
+# run the tests
+docker-compose -f $LOCAL_DIR/.docker/docker-compose-test.yaml up
