ed210b287e16ccf964743fade96d634a665d6e9d
Theo van den Bogaart <todorus@todorus.com>
2016-10-18 07:49:10 +0200

    proof of concept of tests is working

--- app/regions/test.sh
+++ app/regions/test.sh
@@ -2,14 +2,7 @@
 LOCAL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # make sure we use the latest builds
-docker-compose -f $LOCAL_DIR/.docker/docker-compose-production.yaml build
 docker-compose -f $LOCAL_DIR/.docker/docker-compose-test.yaml build
 
-# run the production container
-docker-compose -f $LOCAL_DIR/.docker/docker-compose-production.yaml up
-
-# give the databse time to start
-sleep 10s
-
 # run the tests
-docker-compose -f $LOCAL_DIR/.docker/docker-compose-test.yaml up
+docker-compose -f $LOCAL_DIR/.docker/docker-compose-test.yaml up --abort-on-container-exit --force-recreate
--- app/regions/tests/test_framework.py
+++ app/regions/tests/test_framework.py
@@ -1,23 +1,13 @@
 import unittest
+import requests
 import app.lib.db.setup as db
 import app.lib.model.region as region
 import tools as utils
-from selenium import webdriver
 
 
 class FetchRegions(unittest.TestCase):
 
-    def setUp(self):
-        print "going"
-        self.driver = webdriver.Firefox()
-        self.driver.manage().timeouts().implicitlyWait(4, TimeUnit.SECONDS);
-
-
-    def tearDown(self):
-        self.driver.quit()
-
-
     def test_hello_world(self):
         print "getting"
-        driver.get("https://0.0.0.0:5000/fetch")
-        self.assertEquals("Hello Fetch!", driver.text)
+        req = requests.get("http://api:5000/fetch")
+        self.assertEquals("Hello Fetch!", req.text)
