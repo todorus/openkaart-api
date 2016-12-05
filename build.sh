b8f28cbc58308d3c58e3f75febd628dd85dcb8b8
Theo van den Bogaart <todorus@todorus.com>
2016-12-05 20:27:37 +0100

    build without cache

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 LOCAL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # build the images needed
-docker-compose -f $LOCAL_DIR/deployment/production.yaml build
+docker-compose -f $LOCAL_DIR/deployment/production.yaml build --no-cache
 
 # push it to the repository
 docker-compose -f $LOCAL_DIR/deployment/production.yaml push
