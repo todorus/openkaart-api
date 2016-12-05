3f7e10d4931efbe31700182256b8667df47af1b1
Theo van den Bogaart <todorus@todorus.com>
2016-12-05 21:24:50 +0100

    split up deployment in the stages init and update

--- build.sh
+++ build.sh
@@ -1,8 +1,10 @@
 # get the local directory to convert relative paths to absolute ones
 LOCAL_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-# build the images needed
+# build and push the images for the api
 docker-compose -f $LOCAL_DIR/deployment/production.yaml build --no-cache
-
-# push it to the repository
 docker-compose -f $LOCAL_DIR/deployment/production.yaml push
+
+# build and push the images for certbot
+docker-compose -f $LOCAL_DIR/deployment/certbot.yaml build --no-cache
+docker-compose -f $LOCAL_DIR/deployment/certbot.yaml push
--- deployment/server-files/deploy.sh
+++ deployment/server-files/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-docker-compose -f certbot.yaml pull
-docker-compose -f certbot.yaml up --force-recreate
-
 docker-compose -f production.yaml pull
 docker-compose -f production.yaml up --force-recreate -d
+
+docker-compose -f certbot-update.yaml pull
+docker-compose -f certbot-update.yaml up --force-recreate
